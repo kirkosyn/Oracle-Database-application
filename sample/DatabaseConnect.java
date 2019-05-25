e59e7fc6fec24c50198d4006c0443868c9aab7f5
kirkosyn <38506431+kirkosyn@users.noreply.github.com>
2019-05-26 00:58:36 +0200

    update connection methods
    disconnect, update query, execute query

--- sample/DatabaseConnect.java
+++ sample/DatabaseConnect.java
@@ -9,27 +9,71 @@ public class DatabaseConnect {
 
     public static Connection connection;
 
-    public DatabaseConnect()
-    {
+    public DatabaseConnect() {
 
     }
 
-    public Connection GetConn() throws SQLException
-    {
+    public Connection GetConn() throws SQLException {
         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
         String user = "KPAWLUK";
         String pwd = "wbd2019";
 
-        try
-        {
+        try {
             connection = DriverManager.getConnection(url, user, pwd);
             return connection;
-        }
-        catch(SQLException ex)
-        {
+        } catch (SQLException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText(ex.toString());
+            alert.show();
         }
 
         return connection;
+    }
+
+    public static void DisconnectConnection() throws SQLException {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (Exception e){
+            throw e;
+        }
+    }
+
+    public static ResultSet ExecuteStatement(String cmd) throws SQLException {
+        ResultSet rs = null;
+        Statement state = null;
+
+        try {
+            state = connection.createStatement();
+            rs = state.executeQuery(cmd);
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (state != null) {
+                state.close();
+            }
+            DisconnectConnection();
+        }
+        return rs;
+    }
+
+    public static void ExecuteUpdateStatement(String cmd) throws SQLException {
+        Statement state = null;
+        try {
+            state = connection.createStatement();
+
+            state.executeUpdate(cmd);
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        } finally {
+            if (state != null)
+                state.close();
+            DisconnectConnection();
+        }
+
     }
 }
