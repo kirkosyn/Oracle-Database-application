ab685c8581a28d9875519cb33392a9b875563eb5
kirkosyn <38506431+kirkosyn@users.noreply.github.com>
2019-05-26 01:05:46 +0200

    updated to new gui

--- sample/Main.java
+++ sample/Main.java
@@ -6,27 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main extends Application {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 500));
+        Parent root = FXMLLoader.load(getClass().getResource("admin_menu.fxml"));
+        primaryStage.setTitle("Menadżer pracowników");
+        primaryStage.setScene(new Scene(root, 770, 400));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
-        Controller controller = new Controller();
-        controller.Initialize();
-
     }
 }
