2e3d6df47f471fd185cfca77d0bb8883b4d980d1
kirkosyn <38506431+kirkosyn@users.noreply.github.com>
2019-06-02 18:31:26 +0200

    documentation

--- sample/Adres.java
+++ sample/Adres.java
@@ -20,50 +20,110 @@ public class Adres {
     public Adres() {
     }
 
+    /**
+     * Pobranie id adresu
+     *
+     * @return id adresu
+     */
     public Integer getId_adresu() {
         return id_adresu;
     }
 
+    /**
+     * Ustawienie id adresu
+     *
+     * @param id_adresu id adresu
+     */
     public void setId_adresu(Integer id_adresu) {
         this.id_adresu = id_adresu;
     }
 
+    /**
+     * Pobranie miasta
+     *
+     * @return miasto
+     */
     public String getMiasto() {
         return miasto;
     }
 
+    /**
+     * Ustawienie miasta
+     *
+     * @param miasto miasto
+     */
     public void setMiasto(String miasto) {
         this.miasto = miasto;
     }
 
+    /**
+     * Pobranie ulicy
+     *
+     * @return ulica
+     */
     public String getUlica() {
         return ulica;
     }
 
+    /**
+     * Ustawienie ulicy
+     *
+     * @param ulica ulica
+     */
     public void setUlica(String ulica) {
         this.ulica = ulica;
     }
 
+    /**
+     * Pobranie nr budynku
+     *
+     * @return nr budynku
+     */
     public String getNr_budynku() {
         return nr_budynku;
     }
 
+    /**
+     * Ustawienie nr budynku
+     *
+     * @param nr_budynku nr budynku
+     */
     public void setNr_budynku(String nr_budynku) {
         this.nr_budynku = nr_budynku;
     }
 
+    /**
+     * Pobranie nr lokalu
+     *
+     * @return nr lokalu
+     */
     public Integer getNr_lokalu() {
         return nr_lokalu;
     }
 
+    /**
+     * Ustawienie nr lokalu
+     *
+     * @param nr_lokalu nr lokalu
+     */
     public void setNr_lokalu(Integer nr_lokalu) {
         this.nr_lokalu = nr_lokalu;
     }
 
+    /**
+     * Pobranie kodu pocztowego
+     *
+     * @return kod pocztowy
+     */
     public String getKod_pocztowy() {
         return kod_pocztowy;
     }
 
+    /**
+     * Ustawienie kodu pocztowego
+     *
+     * @param kod_pocztowy kod pocztowy
+     */
     public void setKod_pocztowy(String kod_pocztowy) {
         this.kod_pocztowy = kod_pocztowy;
     }
